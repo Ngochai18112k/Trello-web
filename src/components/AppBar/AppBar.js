7f42d45ffa4155c2bb905a7485b9022e1faa2898
haivudev <haivu1811@gmail.com>
2021-08-29 21:56:40 +0700

    init navbar

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -1,9 +1,49 @@
 import React from 'react'
 import './AppBar.scss';
+import { Container as BSContainer, Row, Col, InputGroup, FormControl } from 'react-bootstrap';
+import imgLogo from 'resources/images/Trello-logo.png';
 
 function AppBar() {
     return (
-        <nav className="navbar-app">App bar</nav>
+        <nav className="navbar-app">
+            <BSContainer className="trello-container">
+                <Row>
+                    <Col sm={5} xs={12} className="col-no-padding">
+                        <div className="app-actions">
+                            <div className="item all"><i className="fa fa-th" /></div>
+                            <div className="item home"><i className="fa fa-home" /></div>
+                            <div className="item boards"><i className="fa fa-columns" />&nbsp;&nbsp;<strong>Boards</strong></div>
+                            <div className="item search">
+                                <InputGroup className="group-search">
+                                    <FormControl
+                                        className="input-search"
+                                        placeholder="Jump to..."
+                                    />
+                                    <InputGroup.Text className="input-icon-search"><i className="fa fa-search" /></InputGroup.Text>
+                                </InputGroup>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col sm={2} xs={12} className="col-no-padding">
+                        <div className="app-branding text-center">
+                            <a href="#">
+                                <img src={imgLogo} className="top-logo" alt="trello-logo" />
+                            </a>
+                        </div>
+                    </Col>
+                    <Col sm={5} xs={12} className="col-no-padding">
+                        <div className="user-actions">
+                            <div className="item quick"><i className="fa fa-plus-square-o" /></div>
+                            <div className="item news"><i className="fa fa-info-circle" /></div>
+                            <div className="item notification"><i className="fa fa-bell-o" /></div>
+                            <div className="item user-avatar">
+                                <img src="https://picsum.photos/200" alt="avatar" title="avatar" />
+                            </div>
+                        </div>
+                    </Col>
+                </Row>
+            </BSContainer>
+        </nav>
     )
 }
 
--- src/components/BoardBar/BoardBar.js
+++ src/components/BoardBar/BoardBar.js
@@ -1,9 +1,39 @@
 import React from 'react'
 import './BoardBar.scss';
+import { Container as BSContainer, Row, Col } from 'react-bootstrap';
 
 function BoardBar() {
     return (
-        <nav className="navbar-board">App board</nav>
+        <nav className="navbar-board">
+            <BSContainer className="trello-container">
+                <Row>
+                    <Col sm={10} xs={12} className="col-no-padding">
+                        <div className="board-info">
+                            <div className="item board-logo-icon"><i className="fa fa-coffee" />&nbsp;&nbsp;<strong>Trello To do list</strong></div>
+                            <div className="divider"></div>
+
+                            <div className="item board-type">Private workspace</div>
+                            <div className="divider"></div>
+
+                            <div className="item member-avatar">
+                                <img src="https://picsum.photos/200" alt="avatar" title="avatar" />
+                                <img src="https://picsum.photos/200" alt="avatar" title="avatar" />
+                                <img src="https://picsum.photos/200" alt="avatar" title="avatar" />
+                                <img src="https://picsum.photos/200" alt="avatar" title="avatar" />
+                                <img src="https://picsum.photos/200" alt="avatar" title="avatar" />
+                                <span className="more-members">+7</span>
+                                <span className="invite">Invite</span>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col sm={2} xs={12} className="col-no-padding">
+                        <div className="board-actions">
+                            <div className="item menu"><i className="fa fa-ellipsis-h mr-2" />&nbsp;&nbsp;Show menu</div>
+                        </div>
+                    </Col>
+                </Row>
+            </BSContainer>
+        </nav>
     )
 }
 
